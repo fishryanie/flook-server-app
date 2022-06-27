247025a0f8d7f54f78ccc5d53f33d21ea9f06d12
quanphps14761 <qphanquan1998@gmail.com>
2022-06-27 15:30:13 +0700

    123

--- index.js
+++ index.js
@@ -38,5 +38,3 @@ const PORT = process.env.PORT || 8081;
 // if (process.env.NODE_ENV !== 'test') {
 app.listen(PORT , () => console.info('Server is running on port ' + PORT));
 // }
-
-module.exports = app
