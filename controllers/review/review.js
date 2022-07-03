e46dc4d75d39eed0bc04d9f4b3025c9a3d121f3c
Phuoc Sanh <phuocsanhtps@gmail.com>
2022-07-03 14:32:35 +0700

    push review

--- controllers/review/review.js
+++ controllers/review/review.js
@@ -4,43 +4,44 @@ const handleError = require("../../error/HandleError");
 
 
 
-// thêm bình luận
+// thêm review
 const addReview = async (req, res) => {
   const idUser = req.userIsLoggedId;
-  console.log("addReview 1234111111", idUser)
+  // console.log("addReview 1234111111", idUser)
 
-//   if (req.body.content.trim().length === 0) {
-//     res.status(400).send({ message: messages.validateContenComment });
-//   } else {
-//     const review = new models.reviews({ ...req.body, idUser });
-//     review.save()
-//       .then((data) =>
-//         res.status(200).send({
-//           data: data,
-//           status: 200,
-//           messages: messages.CreateSuccessfully,
-//         })
-//       )
-//       .catch((error) => {
-//         console.log(`error ${error}`);
-//         handleError.ServerError(error, res);
-//       });
-//   }
+  if (req.body.content.trim().length === 0) {
+    res.status(400).send({ message: messages.validateContenComment });
+  } else {
+    const review = new models.reviews({ ...req.body, users:idUser });
+    review.save()
+      .then((data) =>
+        res.status(200).send({
+          data: data,
+          status: 200,
+          messages: messages.CreateSuccessfully,
+        })
+      )
+      .catch((error) => {
+        console.log(`error ${error}`);
+        handleError.ServerError(error, res);
+      });
+  }
 };
-// update bình luận
-const updateComment = async (req, res) => {
-  const commentUserId = req.userId;
-  const commentUserIdBody = req.body.idUser;
-  const like = req.params.likeordislike;
-  const commentId = req.params.id;
-  const option = { new: true };
 
-  if (commentUserId === commentUserIdBody) {
+// update review
+const updateReview = async (req, res) => {
+  const userId = req.userIsLoggedId.toHexString();
+  const userIdBody = req.body.users;
+  const reviewId = req.params.id;
+  
+  const option = { new: true };
+  
+  if (userId === userIdBody) {
     try {
-      const Comment = new models.comments({ ...req.body, _id: commentId });
-      const result = await models.comments.findByIdAndUpdate(
-        commentId,
-        Comment,
+      const review = { ...req.body, updateAt:Date.now()};
+      const result = await models.reviews.findByIdAndUpdate(
+        reviewId,
+        review, 
         option
       );
       const response = {
@@ -55,168 +56,188 @@ const updateComment = async (req, res) => {
   } else {
     return res
       .status(404)
-      .send({ messages: "Không thể sửa comment của người khác" });
-  }
-};
-//delete Comment
-const deleteComment = async (req, res) => {
-  const id = req.params.id;
-  const commentUserId = req.userId;
-  const commentUserIdBody = req.body.idUser;
-
-  try {
-    if (commentUserId === commentUserIdBody) {
-      const row = await models.comments.findByIdAndRemove(id).exec();
-      if (!row) {
-        handleError.NotFoundError(id, res);
-      }
-      console.log(messages.DeleteSuccessfully);
-      return res.status(200).send({ messages: messages.DeleteSuccessfully });
-    } else {
-      return res
-        .status(404)
-        .send({ messages: "Không thể xóa comment của người khác" });
-    }
-  } catch (error) {
-    handleError.ServerError(error, res);
-    console.log(error);
+      .send({ messages: "Không thể sửa review của người khác" });
   }
 };
 
-// Like, disLikes and unlike, undislike
-const likeAndDislike = async (req, res) => {
-  const like = req.params.likeAndDislike;
-  const commentId = req.params.id;
-  if (!like || !commentId) {
-    return res.status(400).send({ messages: messages.NotFound });
-  }
-  if (like === "like") {
-    console.log(req.body.likes);
+//delete review
+const deleteReview = async (req, res) => {
+  const userId = req.userIsLoggedId.toHexString();
+  const userIdBody = req.body.users;
+  const reviewId = req.params.id;
+  
+  const option = { new: true };
+  
+  if (userId === userIdBody) {
     try {
-      const result = await models.comments.updateOne(
-        { _id: commentId },
-        {
-          $addToSet: {
-            likes: req.body.likes,
-          },
-        }
+      const review = { ...req.body, deleteAt:Date.now()};
+      const result = await models.reviews.findByIdAndUpdate(
+        reviewId,
+        review, 
+        option
       );
-      result.modifiedCount === 0
-        ? res.status(400).send({ messages: "Bạn đã like" })
-        : res.status(200).send({ data: result, messages: "Like successfully" });
-    } catch (error) {
-      handleError.ServerError(error, res);
-      console.log(`error ${error}`);
-    }
-  } else if (like === "dislike") {
-    console.log(req.body.likes);
-    try {
-      const result = await models.comments.updateOne(
-        { _id: commentId },
-        {
-          $addToSet: {
-            disLikes: req.body.disLikes,
-          },
-        }
-      );
-      result.modifiedCount === 0
-        ? res.status(400).send({ messages: "Bạn đã dislike" })
-        : res
-            .status(200)
-            .send({ data: result, messages: "Dislike successfully" });
-    } catch (error) {
-      handleError.ServerError(error, res);
-    }
-  } else if (like === "unlike") {
-    console.log(req.body.likes);
-    try {
-      const result = await models.comments.updateOne(
-        { _id: commentId },
-
-        { $pull: { likes: req.body.likes } }
-      );
-      if (result.modifiedCount === 0) {
-        return res.status(404).send({ messages: "Không tìm thấy id" });
-      }
-      return res
-        .status(200)
-        .send({ data: result, messages: "unlike successfully" });
+      const response = {
+        data: result,
+        status: 200,
+        messages: messages.UpdateSuccessfully,
+      };
+      return res.status(200).send(response);
     } catch (error) {
       handleError.ServerError(error, res);
     }
   } else {
-    console.log(req.body.likes);
-    try {
-      const result = await models.comments.updateOne(
-        { _id: commentId },
-
-        { $pull: { disLikes: req.body.disLikes } }
-      );
-      if (result.modifiedCount === 0) {
-        return res.status(404).send({ messages: "Không tìm thấy id" });
-      }
-
-      return res.status(200).send({ messages: "undislike successfully" });
-    } catch (error) {
-      handleError.ServerError(error, res);
-    }
+    return res
+      .status(404)
+      .send({ messages: "Không thể xóa review của người khác" });
   }
 };
-// danh sách bình luận theo mã phim sắp xếp theo ngày
-const getAllCommentSort = async (req, res) => {
-  const movieId = req.query.idMovie;
-  const sort = req.query.sortComment;
+
+
+// danh sách review theo mã comic sắp xếp theo ngày
+const getAllReviewSort = async (req, res) => {
+  const comicId = req.query.idComic;
+  const sort = req.query.sortReview;
   const rating = req.query.rating;
-  const like = req.query.like;
   let page = parseInt(req.query.page);
   const PAGE_SIZE = 5;
   page < 0 ? (page = 1) : (page = page);
   const skip = (page - 1) * PAGE_SIZE;
-  if (!movieId) {
+
+  try {
+  
+  } catch (error) {
+    
+  }
+
+  if ( !comicId) {
     return res.status(400).send({ messages: messages.NotFound });
   } else {
-    if (page && !sort) {
+    if (page && !sort && !rating) {
       console.log("get all phân trang");
-      const result = await models.comments.find({ idMovie: movieId })
-        .populate(["idMovie", "likes", "disLikes"])
+      try {
+        const result = await models.reviews.find({ ebooks: comicId , deleted:false})
         .skip(skip)
         .limit(PAGE_SIZE);
       return res.status(200).send({ data: result });
-    } else if (page && sort) {
+      } catch (error) {
+        handleError.ServerError(error, res);
+      }
+     
+    } 
+    else if (page && sort && !rating) {
       console.log("Sắp xếp");
-      const result = await models.comments.find({ idMovie: movieId })
-        .populate(["idMovie", "likes", "disLikes"])
+      try {
+        const result = await models.reviews.find({ ebooks: comicId ,deleted:false})
         .skip(skip)
         .limit(PAGE_SIZE)
         .sort({ createAt: sort === "ASC" ? 1 : -1 });
       return res.status(200).send({ data: result });
-    } else if (rating) {
-      console.log(`rating ${rating}`);
-      const result = await models.comments.find({ idMovie: movieId, rating })
-        .populate(["idMovie", "likes", "disLikes"])
-        .limit(10);
-      return res.status(200).send({ data: result });
-    } else {
-      console.log(`like ${like}`);
-      let result = await models.comments.find({ idMovie: movieId }).populate([
-        "idMovie",
-        "likes",
-        "disLikes",
-      ]);
-      result = result.sort((a, b) => b.likes.length - a.likes.length);
-
-      let ketqua = [];
-      for (let i = 0; i < 2; i++) {
-        ketqua.push(result[i]);
+      } catch (error) {
+        handleError.ServerError(error, res);
       }
-      return res.status(200).send({ data: ketqua });
+     
+    } 
+    else if (page && rating && sort) {
+      console.log(`rating ${rating}`);
+
+      try {
+        const result = await models.reviews.find({ ebooks: comicId, deleted:false })
+        .skip(skip)
+        .limit(PAGE_SIZE)
+        .sort({ rating: sort === "ASC" ? 1 : -1 });;
+      return res.status(200).send({ data: result });
+      } catch (error) {
+        handleError.ServerError(error, res);
+        
+      }
+      
     }
+    else return res.status(400).send({ messages: messages.NotFound });
   }
 };
+
+// Like, disLikes and unlike, undislike
+// const likeAndDislike = async (req, res) => {
+//   const like = req.params.likeAndDislike;
+//   const commentId = req.params.id;
+//   if (!like || !commentId) {
+//     return res.status(400).send({ messages: messages.NotFound });
+//   }
+//   if (like === "like") {
+//     console.log(req.body.likes);
+//     try {
+//       const result = await models.comments.updateOne(
+//         { _id: commentId },
+//         {
+//           $addToSet: {
+//             likes: req.body.likes,
+//           },
+//         }
+//       );
+//       result.modifiedCount === 0
+//         ? res.status(400).send({ messages: "Bạn đã like" })
+//         : res.status(200).send({ data: result, messages: "Like successfully" });
+//     } catch (error) {
+//       handleError.ServerError(error, res);
+//       console.log(`error ${error}`);
+//     }
+//   } else if (like === "dislike") {
+//     console.log(req.body.likes);
+//     try {
+//       const result = await models.comments.updateOne(
+//         { _id: commentId },
+//         {
+//           $addToSet: {
+//             disLikes: req.body.disLikes,
+//           },
+//         }
+//       );
+//       result.modifiedCount === 0
+//         ? res.status(400).send({ messages: "Bạn đã dislike" })
+//         : res
+//             .status(200)
+//             .send({ data: result, messages: "Dislike successfully" });
+//     } catch (error) {
+//       handleError.ServerError(error, res);
+//     }
+//   } else if (like === "unlike") {
+//     console.log(req.body.likes);
+//     try {
+//       const result = await models.comments.updateOne(
+//         { _id: commentId },
+
+//         { $pull: { likes: req.body.likes } }
+//       );
+//       if (result.modifiedCount === 0) {
+//         return res.status(404).send({ messages: "Không tìm thấy id" });
+//       }
+//       return res
+//         .status(200)
+//         .send({ data: result, messages: "unlike successfully" });
+//     } catch (error) {
+//       handleError.ServerError(error, res);
+//     }
+//   } else {
+//     console.log(req.body.likes);
+//     try {
+//       const result = await models.comments.updateOne(
+//         { _id: commentId },
+
+//         { $pull: { disLikes: req.body.disLikes } }
+//       );
+//       if (result.modifiedCount === 0) {
+//         return res.status(404).send({ messages: "Không tìm thấy id" });
+//       }
+
+//       return res.status(200).send({ messages: "undislike successfully" });
+//     } catch (error) {
+//       handleError.ServerError(error, res);
+//     }
+//   }
+// };
 module.exports = {
   addReview,
-  updateComment,
-  likeAndDislike,
-  deleteComment,
-  getAllCommentSort,
+  updateReview,
+  deleteReview,
+  getAllReviewSort,
 };
--- routes/review/review.js
+++ routes/review/review.js
@@ -7,21 +7,22 @@ module.exports = app => {
   middlewares.auth.accessPermission("addReview"),
   Controller.review.addReview);
 
-  // update bình luận
-  app.put("/api/comment-management/updateComment/:id",[
-  ],Controller.comment.updateComment);
+  // update review
+  app.put("/api/review-management/updateReview/:id",[
+    middlewares.auth.accessPermission("updateReview"),
+  ],Controller.review.updateReview);
 
   // delete comment
-  app.delete("/api/comment-management/deleteComment/:id",[
+  app.put("/api/review-management/deleteReview/:id",[
+    middlewares.auth.accessPermission("deleteReview"),
+  ],Controller.review.deleteReview);
+  // // like and dislike
+  // app.put("/api/comment-management/likeAndDislikeComment/:id/:likeAndDislike",[
 
-  ],Controller.comment.deleteComment);
-  // like and dislike
-  app.put("/api/comment-management/likeAndDislikeComment/:id/:likeAndDislike",[
+  // ],Controller.comment.likeAndDislike);
 
-  ],Controller.comment.likeAndDislike);
+  // danh sách review theo comic sắp xếp theo ngày
+  app.get("/api/review-management/getAllReviewSort",[
 
-  // danh sách bình luận theo mã phim sắp xếp theo ngày
-  app.get("/api/comment-management/getAllCommentSort",[
-
-  ],Controller.comment.getAllCommentSort);
+  ],Controller.review.getAllReviewSort);
 };
