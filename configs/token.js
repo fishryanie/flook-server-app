82fe14e45de28d40e869d402e6ad73c050c433e0
Phan Hồng Quân <77969603+fishryanie@users.noreply.github.com>
2022-07-12 02:35:42 +0700

    Delete configs directory

--- configs/token.js
+++
@@ -1,9 +0,0 @@
-const FormatDate = require('../functions/FormatDate')
-
-const configsToken = {
-  secret: "bezkoder-secret-key",
-  jwtExpiration: FormatDate.oneHour,
-  jwtRefreshExpiration: FormatDate.oneDay,
-}
-
-module.exports = configsToken
