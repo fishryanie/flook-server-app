3a5a05ca39bd52911f62e5a273584c7ef2c7146a
tuhoang99 <tuhoang.121299@gmail.com>
2022-08-27 22:56:09 +0700

    up code

--- controllers/auth/user.js
+++ controllers/auth/user.js
@@ -167,7 +167,7 @@ module.exports = {
         { isActive: true },
         { new: true, upsert: true }
       );
-      return res.status(200).render(process.cwd() + '/views/isactive.hbs');
+      return res.sendFile(process.cwd() + '/views/isactive.hbs');
     } catch (error) {
       return handleError.ServerError(error, res)
     }
